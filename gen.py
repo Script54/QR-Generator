c8251983c185015394293b11e8d533ccb6680a2f
Script <82963408+Script54@users.noreply.github.com>
2023-06-12 22:57:45 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,11 +1,12 @@
-import pyqrcode 
-from pyqrcode import QRCode 
-  
-# String which represent the QR code 
-s = "https://github.com/Script54"
-  
-# Generate QR code 
-url = pyqrcode.create(s) 
-  
-# Create and save the png file naming "myqr.png" 
-url.svg("myyoutube.svg", scale = 8) 
+import qrcode
+# Link for website
+input_data = ""
+#Creating an instance of qrcode
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
